c1ffee2d37e2bb04e4674696e5596fee74b138e7
Abdullayev2005 <108843760+Abdullayev2005@users.noreply.github.com>
2026-08-15 22:21:08 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "kun60-v1";
+const CACHE = "jurnal-v3";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
